155451eb8966ae33d6ce10a4a489c16c5d4b3055
Ching Hsiang Chen <chingyg@localhost.localdomain>
2016-04-16 23:04:13 -0400

    more exercises

--- functions.js
+++ functions.js
@@ -2,7 +2,9 @@ function main2()
 {
 	//functions1(32243);
 	//functions2("nurses run")
-	functions5("hello my name is ching");
+	//functions7("The quick brown fox");
+	//functions8(1);
+	functions9([]);
 }
 
 /**
@@ -100,4 +102,107 @@ function functions5(string)
 	}
 
 	console.log(words.join(" "));
+}
+
+/**
+ * Write a JavaScript function that accepts a string as a parameter and find the longest word within the string
+ * @return {[type]} [description]
+ */
+function functions6(string)
+{
+	var words = string.split(" ");
+
+	var index = [];
+	var maxLength = 0;
+
+	for(var i = 0; i < words.length; i++)
+	{	
+		//case when you have multiple words with maximum length
+		if(words[i].length == maxLength)
+		{
+			index.push(i);
+		}
+		if(words[i].length > maxLength)
+		{
+			maxLength = words[i].length;
+
+			//clear the array of indexes
+			index = [];
+			index.push(i);
+		}
+
+		
+	}
+
+	console.log("The longest word in: " + string + " are: ");
+	for(var i = 0; i < index.length; i ++)
+	{
+		console.log(words[index[i]]);
+	}
+	
+}
+
+/**
+ * Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+ * @return {[type]} [description]
+ */
+function functions7(string)
+{
+	//number of vowels
+	var count = 0;
+
+	var vowels = "aeiou";
+
+	for(var i = 0; i < string.length; i++)
+	{
+		if (vowels.indexOf(string[i].toLowerCase()) !== -1)
+		{
+			count++;
+		}
+	}
+
+	console.log(string + " has " + count + " vowels");
+}
+
+/**
+ * Write a JavaScript function that accepts a number as a parameter and check the number is prime or not
+ * @return {[type]} [description]
+ */
+function functions8(x)
+{
+	if(x % 2 == 0)
+	{
+		console.log("Not prime");
+	}
+	else if (Math.sqrt(x) % 1 == 0)
+	{
+		console.log("Not prime");
+	}
+	else if(x % 3 == 0)
+	{
+		console.log("Not prime");
+	}
+	else if(x % 5 == 0)
+	{
+		console.log("Not prime");
+	}
+	else if (x % 7 == 0)
+	{
+		console.log("Not prime");
+	}
+	else
+	{
+		console.log("Is prime");
+	}
+
+
+}
+
+/**
+ * Write a JavaScript function which accepts an argument and returns the type.
+ * @return {[type]} [description]
+ */
+function functions9(arg)
+{	
+	console.log(arg.toString() + " type is: " + typeof arg);
 }
